b9dc90fe1f7ca9eefea36d4a4684a187d3bac32d
NZ <n.prokopyk@gmail.com>
2024-08-04 15:00:55 +0200

    [api contacts] react query  and get contacts request added

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,8 +1,20 @@
 import { Link } from "react-router-dom";
-import { contacts } from "../data";
+// import { contacts } from "../data";
+import { fetchContacts } from "../api/contacts";
 import ContactListItem from "./ContactListItem";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ContactList() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["contactsList"],
+    queryFn: fetchContacts,
+  });
+  console.log({ isPending, error, data, isFetching });
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  const contacts = data.resources;
   return (
     <div className="ContactList text-3xl">
       <h2 className="mb-5 pt-6 text-3xl">Contacts</h2>
--- src/components/ContactListItem.jsx
+++ src/components/ContactListItem.jsx
@@ -1,6 +1,13 @@
 import PropTypes from "prop-types";
 
 export default function ContactListItem(props) {
+  const getFieldValue = (fields, key) => {
+    if (!fields[key]) return "";
+    if (!fields[key][0]) return "";
+
+    return fields[key][0].value;
+  };
+
   return (
     <div className="ContactListItem relative flex gap-4 rounded bg-slate-300 p-2.5">
       <div className="flex min-w-14 items-center justify-center">
@@ -13,14 +20,14 @@ export default function ContactListItem(props) {
       <div className="flex flex-col gap-2.5">
         <div className="flex flex-row gap-1">
           <span className="text-xl font-medium text-gray-900 dark:text-white">
-            {props.contactData.fields.first_name.value}
+            {getFieldValue(props.contactData.fields, "first name")}
           </span>
           <span className="text-xl font-medium text-gray-900 dark:text-white">
-            {props.contactData.fields.last_name.value}
+            {getFieldValue(props.contactData.fields, "last name")}
           </span>
         </div>
-        <p className="text-sm text-gray-500 dark:text-gray-400">
-          {props.contactData.fields.email.value}
+        <p className="mb-4 text-sm text-gray-500 dark:text-gray-200">
+          {getFieldValue(props.contactData.fields, "email")}
         </p>
         <div className="flex flex-wrap gap-2">
           {props.contactData.tags.map((tag) => (
@@ -43,15 +50,21 @@ export default function ContactListItem(props) {
 ContactListItem.propTypes = {
   contactData: PropTypes.shape({
     fields: PropTypes.shape({
-      first_name: PropTypes.shape({
+      "first name": PropTypes.arrayOf(
+        PropTypes.shape({
+        value: PropTypes.string.isRequired,
+        }),
+      ),
+      "last name": PropTypes.arrayOf(
+        PropTypes.shape({
+        value: PropTypes.string.isRequired,
+        }),
+      ),
+      email: PropTypes.arrayOf(
+        PropTypes.shape({
         value: PropTypes.string.isRequired,
       }).isRequired,
-      last_name: PropTypes.shape({
-        value: PropTypes.string.isRequired,
-      }).isRequired,
-      email: PropTypes.shape({
-        value: PropTypes.string.isRequired,
-      }).isRequired,
+      ),
     }).isRequired,
     avatar_url: PropTypes.string.isRequired,
     tags: PropTypes.arrayOf(
